0ac7cde136e36849cbce23867840890f4af2def9
Karlosssg <carlos.galvezrobleno@gmail.com>
2025-01-23 19:55:59 +0100

    Add files via upload
    En este script está el código para generar el dataframe de trabajo

---
+++ ACTIVIDAD GRUPAL ALGORITMOS/ALGORITMOS_ACTIVIDAD G_CGR.R
@@ -0,0 +1,30 @@
+rm(list=ls())
+
+path <- '/Users/Karlos/OneDrive - Universidad Rey Juan Carlos/MÁSTER BIOINFORMÁTICA/TEMARIO/MASTER/ALGORTIMOS E IA/ARCHIVOS/ACTIVIDAD GRUPAL'
+setwd(path)
+
+library(readr)
+
+classes_df <- read_delim("classes.csv", col_names = c("Sample", "Class"), delim = ";", show_col_types = FALSE)
+
+
+# Leer el archivo de nombres de columnas
+column_names <- readLines("column_names.txt")
+column_names
+
+# Leer el archivo expression_values.csv
+expression_genes <- read_delim("gene_expression.csv", delim = ";", show_col_types = FALSE)
+
+# Asignar los nombres de las columnas del archivo column_names.txt al dataframe de valores de expresión
+colnames(expression_genes) <- column_names
+
+# Existe discrepancia entre los dataframes classes_df y expression_genes
+
+# Eliminar la fila sample_0 en classes_df 
+classes_df_depurada <- classes_df[classes_df$Sample != "sample_0", ]
+
+# Combinar los dataframes
+combined_df <- cbind(classes_df_depurada, expression_genes)
+
+# Verificar el contenido del dataframe combinado
+head(combined_df)
